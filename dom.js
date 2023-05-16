04e4555bee986ccb9f0ccfe615e078c84399b29b
Vishal Pathania <123644532+VishuPathania@users.noreply.github.com>
2023-05-16 18:34:18 +0530

    Update dom.js
    commit

--- dom.js
+++ dom.js
@@ -5,17 +5,19 @@ async function saveToLocalStorage(event) {
       const price = event.target.price.value;
       const description = event.target.description.value;
       const quantity = event.target.quantity.value;
-
-      const expense = {
+      //const id= 1;
+       
+      const obj = {
           candy: candy,
           price: price,
           description: description,
           quantity: quantity
+          //id:candy
       }
 
-      const response = await axios.post("https://crudcrud.com/api/faa65c25ed3848ef987b193269348517/candyData", expense);
+      const response = await axios.post("https://crudcrud.com/api/eeea988d302e4e1fb3a2b3fc8543c7e5/candyData", obj);
       console.log(response);
-      showNewcandyOnScreen(response.data);
+      showNewCandyOnScreen(response.data);
   } catch(err) {
       console.log(err);
   }
@@ -23,47 +25,49 @@ async function saveToLocalStorage(event) {
 
 window.addEventListener('DOMContentLoaded', async () => {
   try {
-      const response = await axios.get("https://crudcrud.com/api/faa65c25ed3848ef987b193269348517/candyData");
+      const response = await axios.get("https://crudcrud.com/api/eeea988d302e4e1fb3a2b3fc8543c7e5/candyData");
       for (let i = 0; i < response.data.length; i++) {
-        showNewcandyOnScreen(response.data[i]);
+          //showNewCandyOnScreen(response.data[i]);
+          await axios.delete(`https://crudcrud.com/api/eeea988d302e4e1fb3a2b3fc8543c7e5/candyData/${response.data[i]._id}`)
       }
   } catch(err) {
       console.log(err);
   }
 });
 
-function showNewcandyOnScreen(expense){
+function showNewCandyOnScreen(obj) {
   document.getElementById('candy').value = '';
   document.getElementById('price').value = '';
   document.getElementById('description').value = '';
   document.getElementById('quantity').value = '';
 
-  if(document.getElementById(expense.description) !== null){
-    removeCandyFromScreen(expense.description);
+  if (document.getElementById(obj.description) !== null) {
+      removeCandyFromScreen(obj.description);
   }
 
   const parentNode = document.getElementById('listofitems');
   const childElem = document.createElement('li');
-  childElem.textContent = expense.candy + ' - ' + expense.price + ' - ' + expense.description + ' - ' + expense.quantity ;
-  childElem.id = expense.description;
+  childElem.textContent = `${obj.candy} - ${obj.price} - ${obj.description} - ${obj.quantity}`;
+  childElem.id = obj.description;
+
   const editButton = document.createElement('button');
   editButton.textContent = 'Edit';
   editButton.classList.add('btn', 'btn-info', 'ml-2');
   editButton.addEventListener('click', () => {
-    editCandyDetails(expense.candy, expense.price, expense.description, expense.quantity);
+      editCandyDetails(obj.candy, obj.price, obj.description, obj.quantity);
   });
+
   const deleteButton = document.createElement('button');
   deleteButton.textContent = 'Delete';
   deleteButton.classList.add('btn', 'btn-danger', 'ml-2');
   deleteButton.addEventListener('click', () => {
-    deleteCandy(expense.description);
+      deleteCandy(obj.description);
   });
+
   childElem.appendChild(editButton);
   childElem.appendChild(deleteButton);
   parentNode.appendChild(childElem);
 }
-
-// edit function
 
 function editCandyDetails(candy, price, description, quantity) {
   document.getElementById('candy').value = candy;
@@ -74,20 +78,18 @@ function editCandyDetails(candy, price, description, quantity) {
   deleteCandy(description);
 }
 
-//delete function
-
 async function deleteCandy(description) {
-  try{
-    await axios.delete(`https://crudcrud.com/api/faa65c25ed3848ef987b193269348517/candyData/${description}`);
-    removeCandyFromScreen(description);
-  } catch(err){
-    console.log(err);
+  try {
+      await axios.delete(`https://crudcrud.com/api/eeea988d302e4e1fb3a2b3fc8543c7e5/candyData/${description}`);
+      removeCandyFromScreen(description);
+  } catch(err) {
+      console.log(err);
   }
 }
 
-function removeCandyFromScreen(description){
+function removeCandyFromScreen(description) {
   const childNodeToBeDeleted = document.getElementById(description);
-  if(childNodeToBeDeleted){
-    childNodeToBeDeleted.remove();
+  if (childNodeToBeDeleted) {
+      childNodeToBeDeleted.remove();
   }
-} 
+}
